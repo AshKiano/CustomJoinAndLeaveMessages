8283724cac5ce08cb8752587cadbdac4a220d37c
jvyskocil <ash.kiano@gmail.com>
2024-03-31 11:01:17 +0200

    version 1.3

--- src/main/java/com/ashkiano/customjoinandleavemessages/CustomJoinAndLeaveMessages.java
+++ src/main/java/com/ashkiano/customjoinandleavemessages/CustomJoinAndLeaveMessages.java
@@ -5,6 +5,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public final class CustomJoinAndLeaveMessages extends JavaPlugin implements Listener {
 
@@ -16,6 +22,8 @@ public final class CustomJoinAndLeaveMessages extends JavaPlugin implements List
         Metrics metrics = new Metrics(this, 21175);
 
         this.getLogger().info("Thank you for using the CustomJoinAndLeaveMessages plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+
+        checkForUpdates();
 
         // Register the current class as an event listener
         getServer().getPluginManager().registerEvents(this, this);
@@ -33,5 +41,45 @@ public final class CustomJoinAndLeaveMessages extends JavaPlugin implements List
         // Get the custom quit message from config.yml, replacing %player% with the player's name
         String quitMessage = getConfig().getString("quit-message").replace("%player%", event.getPlayer().getName());
         event.setQuitMessage(quitMessage);
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 }
